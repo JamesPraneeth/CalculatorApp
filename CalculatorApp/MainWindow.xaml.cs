d3ede181069b6bb68e2a7264fea7d3529fa1bcb5
Praneeth Tiragabathina <praneeththiragabathina123@gmail.com>
2026-04-22 23:08:56 +0530

    Calculator app completed.

--- CalculatorApp/MainWindow.xaml.cs
+++ CalculatorApp/MainWindow.xaml.cs
@@ -1,24 +1,43 @@
-﻿using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CalculatorApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        double lastNumber, result;
+        string selectedOperator;
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (txtDisplay.Text == "0") txtDisplay.Text = "";
+            txtDisplay.Text += btn.Content.ToString();
+        }
+        private void Operator_Click(object sender, RoutedEventArgs e)
+        {
+            lastNumber = double.Parse(txtDisplay.Text);
+            selectedOperator = ((Button)sender).Content.ToString();
+            txtDisplay.Text = "0";
+        }
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            txtDisplay.Text = "0";
+        }
+        private void Equals_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber = double.Parse(txtDisplay.Text);
+
+            if (selectedOperator == "+") result = lastNumber + newNumber;
+            if (selectedOperator == "-") result = lastNumber - newNumber;
+            if (selectedOperator == "*") result = lastNumber * newNumber;
+            if (selectedOperator == "/") result = lastNumber / newNumber;
+
+            txtDisplay.Text = result.ToString();
         }
     }
 }
